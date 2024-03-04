23e0fc7280c1f82ffa14df2cde58fddb251deec7
Vitaliy Voloshyn <vitaliy.srt1985@gmail.com>
2024-03-04 22:02:47 +0200

    add: - utilities_accounting package; - models; - schemas pydantic; - html 404, base, category, index, navigation;

---
+++ utilities_accounting/models.py
@@ -0,0 +1,110 @@
+from datetime import datetime
+from typing import List
+
+from sqlalchemy import String, ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Provider(Base):
+    __tablename__ = "provider"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(256), unique=True)
+    iban: Mapped[str] = mapped_column(String(29), unique=True, nullable=True)
+    edrpou: Mapped[str] = mapped_column(String(8), nullable=True)
+    icon: Mapped[str] = mapped_column(nullable=True)
+    site: Mapped[str] = mapped_column(nullable=True)
+    deleted: Mapped[bool] = mapped_column(server_default='False')
+    category_id: Mapped[int] = mapped_column(ForeignKey('category.id'))
+    category: Mapped['Category'] = relationship(back_populates='providers')
+    accounts: Mapped[List['Account']] = relationship(back_populates='provider')
+    tariffs: Mapped[List['Tariff']] = relationship(back_populates='provider')
+
+    def __repr__(self) -> str:
+        return f"Provider(id={self.id!r}, name={self.name!r})"
+
+
+class Category(Base):
+    __tablename__ = "category"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), unique=True)
+    url_path: Mapped[str] = mapped_column(unique=True)
+    deleted: Mapped[bool] = mapped_column(server_default='False')
+    providers: Mapped[List["Provider"]] = relationship(back_populates="category", cascade="all, delete-orphan")
+    counters: Mapped[List['Counter']] = relationship(back_populates='category')
+
+    def __repr__(self) -> str:
+        return f"Category(id={self.id!r}, name={self.name!r})"
+
+
+class Counter(Base):
+    __tablename__ = 'counter'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50))
+    date: Mapped[datetime] = mapped_column(server_default=func.current_date())
+    deleted: Mapped[bool] = mapped_column(server_default='False')
+    category_id: Mapped[int] = mapped_column(ForeignKey('category.id'))
+    category: Mapped['Category'] = relationship(back_populates='counters')
+    indicators: Mapped[List['Indicator']] = relationship(back_populates='counter')
+
+    def __repr__(self) -> str:
+        return f"Indicator(id={self.id!r}, name={self.name!r})"
+
+
+class Indicator(Base):
+    __tablename__ = 'indicator'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    value: Mapped[int]
+    date: Mapped[datetime] = mapped_column(server_default=func.current_date())
+    deleted: Mapped[bool] = mapped_column(server_default='False')
+    counter_id: Mapped[int] = mapped_column(ForeignKey('counter.id'))
+    counter: Mapped['Counter'] = relationship(back_populates='indicators')
+
+    def __repr__(self) -> str:
+        return f"Indicator(id={self.id!r}, value={self.value!r})"
+
+
+class Account(Base):
+    __tablename__ = 'account'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    number: Mapped[str]
+    balance: Mapped[float]
+    currency: Mapped[str] = mapped_column(String(3), server_default='UAH')
+    provider_id: Mapped[int] = mapped_column(ForeignKey('provider.id'))
+    deleted: Mapped[bool] = mapped_column(server_default='False')
+    provider: Mapped['Provider'] = relationship(back_populates='accounts')
+    payments: Mapped[List['Payment']] = relationship(back_populates='account')
+
+
+    def __repr__(self) -> str:
+        return f"Account(id={self.id!r}, number={self.number!r})"
+
+
+class Tariff(Base):
+    __tablename__ = 'tariff'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    value: Mapped[float]
+    date: Mapped[datetime] = mapped_column(server_default=func.current_date())
+    provider_id: Mapped[int] = mapped_column(ForeignKey('provider.id'))
+    provider: Mapped['Provider'] = relationship(back_populates='tariffs')
+
+    def __repr__(self) -> str:
+        return f"Tariff(id={self.id!r}, value={self.value!r})"
+
+
+class Payment(Base):
+    __tablename__ = 'payment'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    value: Mapped[float]
+    date: Mapped[datetime] = mapped_column(server_default=func.current_date())
+    account_id: Mapped[int] = mapped_column(ForeignKey('account.id'))
+    account: Mapped['Account'] = relationship(back_populates='payments')
+
+    def __repr__(self) -> str:
+        return f"Tarrif(id={self.id!r}, name={self.value!r})"
---
+++ utilities_accounting/schemas.py
@@ -0,0 +1,95 @@
+from datetime import datetime
+from typing import List, Optional
+
+from pydantic import BaseModel, Field
+
+
+class CategoryDTO(BaseModel):
+    id: int
+    name: str
+    url_path: str
+    deleted: bool = False
+
+
+class CategoryRelDTO(CategoryDTO):
+    providers: List["ProviderDTO"]
+    counters: List["CounterDTO"]
+
+
+class ProviderDTO(BaseModel):
+    id: int
+    name: str
+    iban: Optional[str] = None
+    edrpou: Optional[str] = Field(max_items=8, min_items=8, regex=r"\d{8}")
+    icon: Optional[str] = None
+    site: Optional[str] = None
+    deleted: bool = False
+    category_id: int
+
+
+class ProviderRelDTO(ProviderDTO):
+    category: "CategoryDTO"
+    counters: List["CounterDTO"]
+    tariffs: List["TariffDTO"]
+    payments: List["PaymentDTO"]
+
+
+class CounterDTO(BaseModel):
+    id: int
+    name: str
+    date: datetime
+    deleted: bool = False
+    provider_id: int
+
+
+class CounterRelDTO(CounterDTO):
+    category: 'CategoryDTO'
+    indicators: List['IndicatorDTO']
+
+
+class IndicatorDTO(BaseModel):
+    id: int
+    value: int
+    date: datetime
+    deleted: bool = False
+    counter_id: int
+
+
+class IndicatorRelDTO(IndicatorDTO):
+    counter: "CounterDTO"
+
+
+class AccountDTO(BaseModel):
+    id: int
+    number: str
+    balance: float = Field(decimal_places=2)
+    currency: str = Field(min_items=3, max_items=3, default='UAH')
+    provider_id: int
+    deleted: bool = False
+
+
+class AccountRelDTO(AccountDTO):
+    provider: 'ProviderDTO'
+    payments: List["PaymentDTO"]
+
+
+class TariffDTO(BaseModel):
+    id: int
+    value: float = Field(decimal_places=2)
+    date: datetime
+    provider_id: int
+
+
+class TariffRelDTO(BaseModel):
+    provider: 'ProviderDTO'
+
+
+class PaymentDTO(BaseModel):
+    id: int
+    value: float = Field(decimal_places=2)
+    date: datetime
+    account_id: int
+
+
+class PaymentRelDTO(BaseModel):
+    account: "AccountDTO"
